e3e1e074e51ddc99bd3f011992fcb668f50f10ee
Giovani Abel <abelgiovani@gmail.com>
2019-01-30 22:53:54 -0200

    Primeiro commit - Montada estrutura base

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from .main.controllers import main
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+app.register_blueprint(main)
