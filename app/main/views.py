bc4380bb91952d5b7a9cfe072064d879cc021702
Giovani Abel <abelgiovani@gmail.com>
2019-01-31 08:17:14 -0200

    Adicionada framework Bulma

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__, template_folder='templates')
+main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
 
 @main.route('/')
 def index():
